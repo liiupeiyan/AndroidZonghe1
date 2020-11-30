f4f2dfbb60263209508873625915d0851db96520
shiboliu <860745337@qq.com>
2020-11-30 21:09:11 +0800

    liushibo

---
+++ app/src/main/java/com/example/androidzonghe1/Utils/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.androidzonghe1.Utils;
+
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
