ebbe3e4c2c96179adb33614b2c9a86e4a6c02b40
shiboliu <860745337@qq.com>
2020-12-01 19:37:56 +0800

    liushibo

--- app/src/main/java/com/example/androidzonghe1/activity/yyWork/UsageRulesActivity.java
+++ app/src/main/java/com/example/androidzonghe1/activity/yyWork/UsageRulesActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.androidzonghe1.R;
 
@@ -15,5 +17,20 @@ public class UsageRulesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usage_rules);
         toolbar = findViewById(R.id.toolBar);
+
+        if (getActionBar() != null){
+            getActionBar().hide();
+        }
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("UsageRulesActivity", "toolbar back onClicked");
+                finish();
+            }
+        });
+
+
     }
 }
