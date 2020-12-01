323b6b83bc993a574bf4adcfac2e1438127bca96
shiboliu <860745337@qq.com>
2020-12-01 15:20:00 +0800

    liushibo

--- app/src/main/java/com/example/androidzonghe1/Utils/HttpUtil.java
+++ app/src/main/java/com/example/androidzonghe1/Utils/HttpUtil.java
@@ -1,9 +1,18 @@
 package com.example.androidzonghe1.Utils;
 
 
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class HttpUtil {
 
@@ -13,4 +22,21 @@ public class HttpUtil {
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
+
+    public static void request(String url){
+        HttpUtil.sendOkHttpRequest(url, new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+                Log.e("HttpUtil", "request false");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String json = response.body().toString();
+                //在主线程中修改UI控件
+            }
+        });
+    }
+
 }
