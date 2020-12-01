e0119350243501b136f64b51d140b3099206852c
yangyign <996879525@qq.com>
2020-12-01 18:53:39 +0800

    wallet

--- app/src/main/java/com/example/androidzonghe1/activity/yyWork/WalletDetailsActivity.java
+++ app/src/main/java/com/example/androidzonghe1/activity/yyWork/WalletDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.androidzonghe1.activity.yyWork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,9 +28,9 @@ public class WalletDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wallet_details);
         listView = findViewById(R.id.wallet_list);
         Order order = new Order("河北天客隆",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),299.08, 10.40);
-        Order order1 = new Order("河北天客隆",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),299.08, 10.40);
-        Order order2 = new Order("河北天客隆",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),299.08, 10.40);
-        Order order3 = new Order("河北天客隆",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),299.08, 10.40);
+        Order order1 = new Order("手机话费",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),269.08, 10.40);
+        Order order2 = new Order("美团订单",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),239.08, 10.40);
+        Order order3 = new Order("扫描二维码",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis())),199.08, 10.40);
         wallets.add(order);
         wallets.add(order1);
         wallets.add(order2);
@@ -38,7 +39,12 @@ public class WalletDetailsActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                Intent intent = new Intent(WalletDetailsActivity.this,WalletActivity.class);
+                intent.putExtra("name",wallets.get(position).getOrderName());
+                intent.putExtra("spend",wallets.get(position).getSpend());
+                intent.putExtra("time",wallets.get(position).getTime());
+                intent.putExtra("balance",wallets.get(position).getBalance());
+                startActivity(intent);
             }
         });
     }
