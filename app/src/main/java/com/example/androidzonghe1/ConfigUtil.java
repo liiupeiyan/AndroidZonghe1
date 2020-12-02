b40921f1d556716b3affd7465f1c0d0c0476ef86
xuting <1669259699@qq.com>
2020-12-02 16:32:20 +0800

    xiugai

--- app/src/main/java/com/example/androidzonghe1/ConfigUtil.java
+++ app/src/main/java/com/example/androidzonghe1/ConfigUtil.java
@@ -18,6 +18,9 @@ public class ConfigUtil {
     public static String userName = "";
     //当前手机号
     public static String phone="";
+    //任佳旭连接服务器
+    public static final String Url = "http://10.7.90.67:8080/DingDong/GetChildServlet";
+
 
     public static boolean adapterFlag =  true;
 
---
+++ app/src/main/java/com/example/androidzonghe1/entity/xtWork/Child.java
@@ -0,0 +1,52 @@
+package com.example.androidzonghe1.entity.xtWork;
+
+public class Child {
+    private int id;
+    private String name;
+    private String sex;
+    private String banji;
+    private String school;
+    private String img;
+
+
+    public Child() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getSchool() {
+        return school;
+    }
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    public String getImg() {
+        return img;
+    }
+    public void setImg(String img) {
+        this.img = img;
+    }
+    public String getBanji() {
+        return banji;
+    }
+    public void setBanji(String banji) {
+        this.banji = banji;
+    }
+
+}
+
