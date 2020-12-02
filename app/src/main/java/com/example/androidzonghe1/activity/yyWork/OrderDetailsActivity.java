27f3d2e58ddd0108c35e4e0f9014ef4fe1eab7ba
yangyign <996879525@qq.com>
2020-12-02 09:20:13 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/androidzonghe1/activity/yyWork/OrderDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidzonghe1.activity.yyWork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidzonghe1.R;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+
+    }
+}
