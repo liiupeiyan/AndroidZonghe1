f8bfd54ecbcb32214820669a7a3b84f8782ebb26
yangyign <996879525@qq.com>
2020-12-14 15:54:17 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/androidzonghe1/activity/yyWork/MyTest.java
@@ -0,0 +1,10 @@
+package com.example.androidzonghe1.activity.yyWork;
+
+import android.util.Log;
+
+public class MyTest {
+    public static String getStr(){
+        Log.e("测试","test");
+        return "test";
+    }
+}
